9d0056856db6cd9fc92fb43626df4ec08e1d5121
alakh0703 <alakhHBN007@gmail.com>
2024-01-18 02:41:32 -0500

    Add existing project files to Git

---
+++ app/api/detect-objects/route.ts
@@ -0,0 +1,39 @@
+import { utapi } from "@/utils/uploadthing";
+import { pipeline } from "@xenova/transformers";
+
+export async function POST(req: Request, res: Response) {
+  // get uploaded url from uploadthing
+  const formData = await req.formData();
+  const files = formData.getAll("files");
+  const response = await utapi.uploadFiles(files);
+  const responseData = response[0].data;
+  const url = responseData?.url;
+  console.log(url);
+
+  //dtect objects using onnx local model
+  const detector = await pipeline("object-detection", "Xenova/detr-resnet-50");
+  if(!url) return new Response("No url", { status: 400 });
+  const output = await detector(url);
+  console.log(output);
+
+  // parse output -> list of objects -> label (detected objects)
+
+  const countObj: { [key: string]: number } = {};
+  output.forEach(({ score, label }: any) => {
+    if (score > 0.85) {
+      if (countObj[label]) {
+        countObj[label]++;
+      } else {
+        countObj[label] = 1;
+      }
+    }
+  });
+
+  return new Response(
+    JSON.stringify({
+      url: url,
+      label: JSON.stringify(countObj),
+    }),
+    { status: 200 }
+  );
+}
---
+++ app/image-classification/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+import React, { useState } from "react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import axios from "axios";
+import { ImageIcon, Loader2, ScanSearch } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {};
+interface DetectionResult {
+    [key: string]: number;
+}
+
+const ImageClassificationPage = (props: Props) => {
+    const [url, setUrl] = useState<string>("");
+    const [detectionResult, setDetectionResult] = useState<DetectionResult | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    async function uploadFiles(event: React.FormEvent) {
+        event.preventDefault();
+        const formData = new FormData(event.target as HTMLFormElement);
+        setLoading(true);
+        if (formData.get("files") === null) return alert("Please upload an image");
+
+        try {
+            const response = await axios.post("/api/detect-objects", formData);
+            setUrl(response.data.url);
+            const jsonData = JSON.parse(response.data.label) as DetectionResult;
+            console.log(jsonData)
+            setDetectionResult(jsonData);
+        } catch (error) {
+            alert("Please upload an image");
+            console.error("Error uploading files:", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    return (
+        <main className="flex flex-col items-center justify-start p-8 gap-8 bg-gray-100 h-[100vh]">
+            {/* Left Column: Image Upload and Display */}
+            <div className="flex flex-col items-center gap-4 w-full md:w-2/3 h-full">
+                <form onSubmit={uploadFiles} className="flex flex-col items-center gap-4">
+                    <ImageIcon className="text-blue-500" size={24} />
+                    <Input name="files" type="file" className="p-2 border border-gray-300 rounded" />
+                    <Button disabled={loading} type="submit" className="bg-blue-500 text-white hover:bg-blue-600">
+                        {loading ? <Loader2 className="animate-spin" /> : <ScanSearch size={20} />}
+                    </Button>
+                </form>
+
+                {url && (
+                    <div className="w-full  flex justify-center mt-[50px] md:w-3/4">
+                        <Image src={url} width={400} height={400} alt="uploaded image" className="rounded-lg shadow-md" />
+                        <Link href={url} className={cn(buttonVariants({ variant: "ghost" }), "text-xs text-muted-foreground")}></Link>
+                    </div>
+                )}
+            </div>
+
+            {/* Right Column: Table Display */}
+            {/* Right Column: Table Display */}
+            <div className="flex flex-col justify-center w-full md:w-1/3">
+                {Object.keys(detectionResult || {}).length > 0 && (
+                    <div className="bg-white p-4 rounded-md shadow-md">
+                        <p className="text-xl font-bold mb-4">Detected Labels:</p>
+                        <table className="w-full">
+                            <thead>
+                                <tr>
+                                    <th className="text-left">Label</th>
+                                    <th className="text-left">Count</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Object.entries(detectionResult).map(([label, count]) => (
+                                    <tr key={label}>
+                                        <td>{label}</td>
+                                        <td>{count}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
+
+        </main>
+    );
+};
+
+export default ImageClassificationPage;
